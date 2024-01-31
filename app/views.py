d5691e664b837fbaafdff99a067430fc54d08f25
Sivasai2720 <sivasaikumarkongala@gmail.com>
2024-01-31 09:46:28 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,5 +4,20 @@ from app.models import *
 def equijoins(request):
 
     EMPOBJECTS=Emp.objects.select_related('deptno').all()
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(hiredate__year=1981)
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(sal__gt=1250)
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(comm__isnull=True)
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(comm__isnull=True,sal__lt=1250)
+    EMPOBJECTS=Emp.objects.select_related('deptno').all()
+
+    #------For Writing Conditions-------
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(ename='SMITH')   #syn--> Col name= value,   which do u want, when u writing condtion for Child table
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(deptno__dname='ACCOUNTING')  #syn--> CommonColumn__col name which do u want, when u writing condtion for parent table
+
+
+
+    #--------For Printing the Data----------
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(deptno=10)    #syn--> obj name.colname,   which do u want, when u printing the data for Child table
+    #syn--> Obj name.deptno.Col name, which do u want, when u printing the data for parent table
     d={'EMPOBJECTS':EMPOBJECTS}
     return render(request,'equijoins.html',d)
